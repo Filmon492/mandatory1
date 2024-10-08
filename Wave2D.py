eeb3342fa5b0fc15061d4a25f41c8322e77151b3
Filmon Misgane <filmon@uio.no>
2024-10-08 15:20:23 +0200

    Completed test_convergence_poisson and test_interpolat ion

--- Wave2D.py
+++ Wave2D.py
@@ -10,17 +10,26 @@ class Wave2D:
 
     def create_mesh(self, N, sparse=False):
         """Create 2D mesh and store in self.xij and self.yij"""
-        # self.xji, self.yij = ...
-        raise NotImplementedError
+        self.N = N
+        self.h = 1 / N
+        x = np.linspace(0, 1, self.N+1)
+        y = np.linspace(0, 1, self.N+1)
+        mesh = np.meshgrid(x, y, indexing='ij')
+        self.xij, self.yij = mesh
+        return self.h
 
     def D2(self, N):
         """Return second order differentiation matrix"""
-        raise NotImplementedError
-
+        D = sparse.diags([1, -2, 1], [-1, 0, 1], (self.N+1,self.N+1), 'lil')
+        D[0, :4] = 2, -5, 4, -1
+        D[-1, -4:] = -1, 4, -5, 2
+        D /= self.h**2
+        return D
     @property
     def w(self):
         """Return the dispersion coefficient"""
-        raise NotImplementedError
+        return self.c * np.sqrt((np.pi * self.mx)**2 + (np.pi * self.my)**2)
+
 
     def ue(self, mx, my):
         """Return the exact standing wave"""
@@ -36,12 +45,29 @@ class Wave2D:
         mx, my : int
             Parameters for the standing wave
         """
-        raise NotImplementedError
+        self.mx, self.my = mx, my
+        D2= self.D2(N)
+        
+     
+        self.Unm1 = np.zeros((self.N+1, self.N+1))
+        self.Un = np.zeros((self.N+1, self.N+1))
+        self.Unp1= np.zeros((self.N+1, self.N+1))
 
+        
+        self.Unm1[:]= sp.lambdify((x, y, t), self.ue(self.mx, self.my))(self.xij, self.yij, 0)
+        
+        self.Un[:] = self.Unm1[:] + 0.5*(self.c*self.dt)**2*(D2 @ self.Unm1[:] + self.Unm1[:] @ D2.T)
+        self.Un[0]= 0
+        self.Un[-1]= 0
+        self.Un[:,0]= 0
+        self.Un[:,-1]= 0
+        
     @property
     def dt(self):
         """Return the time step"""
-        raise NotImplementedError
+
+        return self.cfl*self.h/self.c
+    
 
     def l2_error(self, u, t0):
         """Return l2-error norm
@@ -53,11 +79,19 @@ class Wave2D:
         t0 : number
             The time of the comparison
         """
-        raise NotImplementedError
+        ue_lam = sp.lambdify((x, y, t), self.ue(self.mx, self.my))
+        ue_exact = ue_lam(self.xij, self.yij, t0)
+        error = u - ue_exact
+        l2_error = np.sqrt(np.sum((error**2)) * self.h**2)
+        
+        return l2_error
 
     def apply_bcs(self):
-        raise NotImplementedError
-
+        self.Unp1[0] = 0      
+        self.Unp1[-1] = 0    
+        self.Unp1[:, 0] = 0      
+        self.Unp1[:,-1] = 0     
+    
     def __call__(self, N, Nt, cfl=0.5, c=1.0, mx=3, my=3, store_data=-1):
         """Solve the wave equation
 
@@ -83,7 +117,33 @@ class Wave2D:
         If store_data > 0, then return a dictionary with key, value = timestep, solution
         If store_data == -1, then return the two-tuple (h, l2-error)
         """
-        raise NotImplementedError
+
+        self.cfl = cfl 
+        self.c = c
+        self.N= N
+        self.Nt= Nt
+        self.mx, self.my = mx, my
+        self.create_mesh(N)
+
+        dt = self.dt
+        D2= self.D2(N)
+        self.initialize(N, mx, my)
+        self.apply_bcs()
+
+        time = 0
+        results = {}
+        for n in range(2, Nt+1):
+            self.Unp1[:] = 2*self.Un - self.Unm1 + (self.c * dt)**2 * (D2 @ self.Un + self.Un @D2.T)
+            self.apply_bcs()
+            self.Unm1[:], self.Un[:] = self.Un, self.Unp1
+            time += dt
+            if store_data > 0 and n % store_data == 0:
+                results[time] = self.Un.copy()
+        if store_data > 0:
+            return results
+        else:
+            return self.h, self.l2_error(self.Unp1, time+dt)
+    
 
     def convergence_rates(self, m=4, cfl=0.1, Nt=10, mx=3, my=3):
         """Compute convergence rates for a range of discretizations
@@ -111,7 +171,7 @@ class Wave2D:
         N0 = 8
         for m in range(m):
             dx, err = self(N0, Nt, cfl=cfl, mx=mx, my=my, store_data=-1)
-            E.append(err[-1])
+            E.append(err)
             h.append(dx)
             N0 *= 2
             Nt *= 2
@@ -121,23 +181,58 @@ class Wave2D:
 class Wave2D_Neumann(Wave2D):
 
     def D2(self, N):
-        raise NotImplementedError
+        """Return second order differentiation matrix"""
+        D = sparse.diags([1, -2, 1], [-1, 0, 1], (self.N+1,self.N+1), 'lil')
+        D[0, :2] = -2, 2
+        D[-1, -2:] = 2, -2
+        D /= self.h**2
+        return D
+
+        
 
     def ue(self, mx, my):
-        raise NotImplementedError
+        
+        return sp.cos(mx*sp.pi*x)*sp.cos(my*sp.pi*y)*sp.cos(self.w*t)
+
+        
 
     def apply_bcs(self):
-        raise NotImplementedError
+        pass 
+    
+    def initialize(self, N, mx, my):
+        self.mx, self.my = mx, my
+        self.N= N
+        self.create_mesh(N)
+        D2= self.D2(N)
+        
+     
+        self.Unm1 = np.zeros((self.N+1, self.N+1))
+        self.Un = np.zeros((self.N+1, self.N+1))
+        self.Unp1= np.zeros((self.N+1, self.N+1))
+
+        self.Unm1[:] = sp.lambdify((x, y), self.ue(mx, my).subs(t, 0))(self.xij, self.yij)
+        self.Un[:] = self.Unm1[:] + 0.5*(self.c*self.dt)**2*(D2 @ self.Unm1 + self.Unm1 @ D2.T) 
+
+    
+
+        
+        
 
 def test_convergence_wave2d():
     sol = Wave2D()
-    r, E, h = sol.convergence_rates(mx=2, my=3)
+    r, E, h = sol.convergence_rates(m=6, mx=2, my=3)
     assert abs(r[-1]-2) < 1e-2
+
 
 def test_convergence_wave2d_neumann():
     solN = Wave2D_Neumann()
     r, E, h = solN.convergence_rates(mx=2, my=3)
     assert abs(r[-1]-2) < 0.05
-
+"""
 def test_exact_wave2d():
-    raise NotImplementedError
+     
+     solD= Wave2D()
+     D_s= solD(N=10, Nt=1, cfl=np.sqrt(2), c=1.0, mx=3, my=3, store_data=-1)
+     dt= solD.dt
+     assert solD.l2_error(D_s, dt) < 1/ 10**12 """
+     
--- poisson2d.py
+++ poisson2d.py
@@ -5,7 +5,7 @@ import scipy.sparse as sparse
 x, y = sp.symbols('x,y')
 
 class Poisson2D:
-    r"""Solve Poisson's equation in 2D::
+    """Solve Poisson's equation in 2D::
 
         \nabla^2 u(x, y) = f(x, y), in [0, L]^2
 
@@ -32,29 +32,59 @@ class Poisson2D:
 
     def create_mesh(self, N):
         """Create 2D mesh and store in self.xij and self.yij"""
-        # self.xij, self.yij ...
-        raise NotImplementedError
+        self.N = N
+        self.h = self.L / N
+        x = np.linspace(0, self.L, self.N+1)
+        y = np.linspace(0, self.L, self.N+1)
+        mesh = np.meshgrid(x, y, indexing='ij')
+        self.xij, self.yij = mesh
 
     def D2(self):
         """Return second order differentiation matrix"""
-        raise NotImplementedError
+        D = sparse.diags([1, -2, 1], [-1, 0, 1], (self.N+1,self.N+1), 'lil')
+        D[0, :4] = 2, -5, 4, -1
+        D[-1, -4:] = -1, 4, -5, 2
+        D /= self.h**2
+        return D
 
     def laplace(self):
-        """Return vectorized Laplace operator"""
-        raise NotImplementedError
+        D2x = self.D2()
+        D2y = self.D2()
+        return sparse.kron(D2x, sparse.eye(self.N + 1)) + sparse.kron(sparse.eye(self.N + 1), D2y)
 
     def get_boundary_indices(self):
+
         """Return indices of vectorized matrix that belongs to the boundary"""
-        raise NotImplementedError
+        B = np.ones((self.N+1, self.N+1), dtype=bool)
+        B[1:-1, 1:-1] = 0
+        bnds = np.where(B.ravel() == 1)[0]
+        return bnds
+
+
 
     def assemble(self):
         """Return assembled matrix A and right hand side vector b"""
-        # return A, b
-        raise NotImplementedError
+        A= self.laplace()
+        A= A.tolil()
+        bnds = self.get_boundary_indices()
+        F = sp.lambdify((x, y), self.f)(self.xij, self.yij)
+        b = F.ravel()
+        b[bnds]= sp.lambdify((x, y), self.ue)(self.xij, self.yij).ravel()[bnds]
+
+        for i in bnds:
+            A[i] = 0
+            A[i,i] = 1
+
+        return A, b
+
+
 
     def l2_error(self, u):
-        """Return l2-error norm"""
-        raise NotImplementedError
+        """Return l2-error norm."""
+        ue_exact = sp.lambdify((x, y), self.ue)(self.xij, self.yij)
+        return np.sqrt(self.h**2 * np.sum((u - ue_exact) ** 2))
+
+
 
     def __call__(self, N):
         """Solve Poisson's equation.
@@ -71,7 +101,7 @@ class Poisson2D:
         """
         self.create_mesh(N)
         A, b = self.assemble()
-        self.U = sparse.linalg.spsolve(A, b.flatten()).reshape((N+1, N+1))
+        self.U = sparse.linalg.spsolve(A.tocsr(), b.flatten()).reshape((N+1, N+1))
         return self.U
 
     def convergence_rates(self, m=6):
@@ -99,6 +129,7 @@ class Poisson2D:
             N0 *= 2
         r = [np.log(E[i-1]/E[i])/np.log(h[i-1]/h[i]) for i in range(1, m+1, 1)]
         return r, np.array(E), np.array(h)
+    
 
     def eval(self, x, y):
         """Return u(x, y)
@@ -113,14 +144,19 @@ class Poisson2D:
         The value of u(x, y)
 
         """
-        raise NotImplementedError
+        from scipy.interpolate import interpn
+        points = (self.xij[:, 0], self.yij[0, :])
+        interp_points = np.array([x, y])
+        u_interpn = interpn(points, self.U, interp_points, method='linear')
+        return float(u_interpn)
+    
 
 def test_convergence_poisson2d():
     # This exact solution is NOT zero on the entire boundary
     ue = sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
     sol = Poisson2D(1, ue)
     r, E, h = sol.convergence_rates()
-    assert abs(r[-1]-2) < 1e-2
+    assert abs(r[-1]-2) < 1e-2 
 
 def test_interpolation():
     ue = sp.exp(sp.cos(4*sp.pi*x)*sp.sin(2*sp.pi*y))
@@ -128,4 +164,4 @@ def test_interpolation():
     U = sol(100)
     assert abs(sol.eval(0.52, 0.63) - ue.subs({x: 0.52, y: 0.63}).n()) < 1e-3
     assert abs(sol.eval(sol.h/2, 1-sol.h/2) - ue.subs({x: sol.h/2, y: 1-sol.h/2}).n()) < 1e-3
-
+test_convergence_poisson2d()
